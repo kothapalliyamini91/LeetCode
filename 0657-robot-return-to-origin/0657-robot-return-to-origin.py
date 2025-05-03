53cadec17c9f2f070d8e35af8508e38faf85076b
Kothapalli Yamini <105288811+kothapalliyamini91@users.noreply.github.com>
2025-05-03 16:16:30 +0530

    Time: 23 ms (15.92%), Space: 17.8 MB (63.11%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.py
@@ -0,0 +1,11 @@
+class Solution:
+    def judgeCircle(self, moves):
+        x,y = 0, 0
+        for char in moves:
+            if char=="R": x+= 1
+            if char=="L": x-= 1
+            if char=="U": y+= 1
+            if char=="D": y-= 1
+        return True if x==0 and y==0 else False
+
+        
